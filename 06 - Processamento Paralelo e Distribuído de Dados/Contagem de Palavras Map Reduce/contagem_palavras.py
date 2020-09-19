8208f401e6e5e7ceb28583151835952d29f87ab1
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2020-09-19 00:13:23 -0300

    Corrige algoritmo e gera CSV com contagem de palavras

--- 06 - Processamento Paralelo e Distribuído de Dados/Contagem de Palavras Map Reduce/contagem_palavras.py
+++ 06 - Processamento Paralelo e Distribuído de Dados/Contagem de Palavras Map Reduce/contagem_palavras.py
@@ -3,13 +3,12 @@
 import csv
 import glob
 import sys
-
 import mincemeat
-from stopwords import allStopWords
 
-caminho_arquivos = "C:\\Users\\Lord_Negrisoli\\Desktop\\Faculdade\\Pós-Graduação\\Repositórios\\06 - Processamento Paralelo e Distribuído de Dados\\Contagem de Palavras Map Reduce\\textos\\*"
+arquivos = glob.glob("textos\\*")
 
-arquivos = glob.glob(caminho_arquivos)
+print(arquivos)
+
 
 def conteudo_arquivos(arquivo):
     arquivo_leitura = open(arquivo)
@@ -18,25 +17,30 @@ def conteudo_arquivos(arquivo):
     finally:
         arquivo_leitura.close()
 
+
 fonte = dict((arquivo, conteudo_arquivos(arquivo)) for arquivo in arquivos)
 
+
 def mapfn(k, v):
+    from stopwords import allStopWords
     print('map {}'.format(k))
     for line in v.splitlines():
         for word in line.split():
             if (word not in allStopWords):
                 yield word, 1
 
+
 def reducefn(k, v):
-    print('Reduce {}'.format(k))
+    print('reduce ' + k)
     return sum(v)
+
 
 s = mincemeat.Server()
 s.datasource = fonte
 s.mapfn = mapfn
 s.reducefn = reducefn
 
-results = s.run_server(password = "123456")
+results = s.run_server(password="123456")
 
 w = csv.writer(open("resultados/results.csv", "w"))
 for k, v in results.items():
